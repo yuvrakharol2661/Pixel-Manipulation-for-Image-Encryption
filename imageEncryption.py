697af4238a0312fc3a8dab4b851051305cd3efd0
yuvrakharol2661 <yuvrajkharol57@gmail.com>
2026-05-13 13:24:27 +0530

    implement image encryption

--- imageEncryption.py
+++ imageEncryption.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 image = cv2.imread("input.jpg")
-key = 50
-encrypted = image + key
+ke = 50
+encrypted = image + ke
 cv2.imwrite("encryption.jpg", encrypted)
 print("Image Encrypted Successfully!")
-decrypted = encrypted - key
+decrypted = encrypted - ke
 cv2.imwrite("decrypted.jpg", decrypted)
 print("Image Decrypted Successfully!")
